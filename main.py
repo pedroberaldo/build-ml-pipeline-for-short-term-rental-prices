aa1fc8141bb1ba011a4af3881ae41caf62276500
Pedro Beraldo <berald@gmail.com>
2023-01-31 19:00:58 -0300

    best model config file. Ready for v1.0.0 release

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 import wandb
 import hydra
 from omegaconf import DictConfig
+import logging
 
 _steps = [
     "download",
@@ -18,7 +19,8 @@ _steps = [
     # then you need to run this step explicitly
 #    "test_regression_model"
 ]
-
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
 
 # This automatically reads in the configuration
 @hydra.main(config_name='config')
@@ -78,9 +80,18 @@ def go(config: DictConfig):
             )
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
+
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                version='main',
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config['modeling']['test_size'],
+                    "random_seed": config['modeling']['random_seed'],
+                    "stratify_by": config['modeling']['stratify_by']
+                },
+            )
             pass
 
         if "train_random_forest" in active_steps:
@@ -90,14 +101,19 @@ def go(config: DictConfig):
             with open(rf_config, "w+") as fp:
                 json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
 
-            # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
-            # step
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+        _ = mlflow.run(
+            os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+            "main",
+            parameters={
+                "trainval_artifact": "nyc_airbnb/trainval_data.csv:v0", #config["data"]["train_data"],
+                "val_size": config["modeling"]["val_size"],
+                "random_seed": config["modeling"]["random_seed"],
+                "stratify_by" : config["modeling"]["stratify_by"],
+                "rf_config" : rf_config,
+                "max_tfidf_features" : config["modeling"]["max_tfidf_features"],
+                "output_artifact" : "random_forest_export"
+            },
+        )
 
         if "test_regression_model" in active_steps:
 
